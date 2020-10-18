537eb28bc661fea8485f9301a615f0445ae9bcb0
Dhuvaraggn <dhuvaraggnajithraj@gmail.com>
2020-10-18 08:58:35 +0530

    make it better

--- web/settings.py
+++ web/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '6y^s==!*=u=-*wjy_!x!!m+tzses)-se4b=8ro0kay5xr_k**c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
